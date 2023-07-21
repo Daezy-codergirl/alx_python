35baebe6b026a982271106f492c29283bd03d22f
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-07-21 16:38:23 +0100

    more prints

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,8 @@
 for first_digit in range(10):
-    for second_digit in range(first_digit - 1):
-        print("{:02d}".format(first_digit+second_digit), end=', ' if second_digit < 10 else '\n')
+    for second_digit in range(first_digit + 1, 10):
+        if first_digit == 8 and second_digit == 9: 
+           print("{:02d}".format(first_digit * 10 + second_digit))
+        else:
+           print("{:02d}, ".format(first_digit * 10 + second_digit), end="") 
+        
+        
