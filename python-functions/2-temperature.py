6115e562dbd4e1bf2e4094bf533c02368bfeba5a
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-07-22 23:43:21 +0100

    this function converts one temp to another

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,7 +2,8 @@ def convert_to_celsius(fahrenheit):
     celcius = (fahrenheit - 32) * 5 / 9
     return celcius
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+if __name__ == "__main__":
+  print(convert_to_celsius(100))
+  print(convert_to_celsius(-40))
+  print(convert_to_celsius(-459.67))
+  print(convert_to_celsius(32))
