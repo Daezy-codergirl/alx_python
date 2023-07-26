bc2f0a42dce71a7653c41f01a7fb6f9304e10557
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-07-26 23:40:52 +0100

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,28 +1,14 @@
 def safe_print_division(a, b):
-    return a / b 
-a = 12
-b = 2
 
-result = safe_print_division(a, b)
-
-
-
-
-print("{:d} / {:d} = {}".format(a, b, result))
-
-   
-if __name__ == "__main__":  
-
- a = 12
- b = 0
- 
  try:
+        result = a / b
         print("Inside result: {}".format(result))
 
  except ZeroDivisionError:
         result = None
  finally:
         print("Inside result: {}".format(result))
+        return result
 
       
 
