fe3b290add7f6b3ec1538ffd8d6842bc52147721
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-07-28 19:42:43 +0100

    squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -14,7 +14,8 @@ matrix = [
     [7, 8, 9]
 
 ]
+if __name__ == "__main__":
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+ new_matrix = square_matrix_simple(matrix)
+ print(new_matrix)
+ print(matrix)
