a9e33f2efef8621ab828bbc003cadd0a6642ef4f
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-04 19:44:27 +0100

    inherits

---
+++ python-inheritance/2-main.py
@@ -0,0 +1,41 @@
+"""
+parameters:
+ obj: any python object.
+ a_class: a python class or class name to compare the type of the object against.object
+
+ returns:
+  boo1: true if the object is an instance of the specified class; otherwise, false.
+ 
+ example:
+>>> class Animal:
+...  pass
+...
+>>> class Dog(Animal):
+... pass
+...
+>>> class Cat(Animal):
+... pass
+...
+>>> class Bird:
+... pass
+...
+>>> obj1 = Dog()
+>>> obj2 = Cat()
+>>> obj3 = Bird()
+>>>
+>>> inherits_from(obj1, Animal)
+True
+>>> inherits_from(obj2, Animal)
+True
+>>> inherits_from(obj3, Animal)
+False
+
+"""
+def inherits_from(obj, a_class):
+    """ check if the given object is exactly an instance of the specified class.
+        also if it is inherited from the specified class
+    """
+    return (
+        issubclass(type(obj), a_class) and type(obj) != a_class
+    )
+   
