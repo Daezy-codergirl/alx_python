157c4998df6b134b249edc673af1b75371a5cc94
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-04 20:50:24 +0100

    inherits

--- python-inheritance/2-main.py
+++ python-inheritance/2-main.py
@@ -1,41 +1,9 @@
-"""
-parameters:
- obj: any python object.
- a_class: a python class or class name to compare the type of the object against.object
+inherits_from = __import__('2-inherits_from').inherits_from
 
- returns:
-  boo1: true if the object is an instance of the specified class; otherwise, false.
- 
- example:
->>> class Animal:
-...  pass
-...
->>> class Dog(Animal):
-... pass
-...
->>> class Cat(Animal):
-... pass
-...
->>> class Bird:
-... pass
-...
->>> obj1 = Dog()
->>> obj2 = Cat()
->>> obj3 = Bird()
->>>
->>> inherits_from(obj1, Animal)
-True
->>> inherits_from(obj2, Animal)
-True
->>> inherits_from(obj3, Animal)
-False
-
-"""
-def inherits_from(obj, a_class):
-    """ check if the given object is exactly an instance of the specified class.
-        also if it is inherited from the specified class
-    """
-    return (
-        issubclass(type(obj), a_class) and type(obj) != a_class
-    )
-   
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
