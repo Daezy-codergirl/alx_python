f3f3fa13084bc940e0d686b6baeb84fb443ed45e
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-04 22:07:59 +0100

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,8 +1,8 @@
 """
-check if the given object is an instance of a class
-also if it is inherited from the specified class
+  check if the given object is an instance of a class
+  also if it is inherited from the specified class
 
-parameters:
+ parameters:
  obj: any python object.
  a_class: a python class or class name to compare the type of the object against.
 
@@ -10,29 +10,29 @@ parameters:
  true if the object is an instance of the subclass of the specified subclass;
  otherwise, false.
  example:
->>> class Animal:
-...  pass
-...
->>> class Dog(Animal):
-... pass
-...
->>> class Cat(Animal):
-... pass
-...
->>> class Bird:
-... pass
-...
->>> obj1 = Dog()
->>> obj2 = Cat()
->>> obj3 = Bird()
->>>
->>> inherits_from(obj1, Animal)
-True
->>> inherits_from(obj2, Animal)
-True
->>> inherits_from(obj3, Animal)
-False
-"""
+   >>> class Animal:
+   ...  pass
+   ...
+   >>> class Dog(Animal):
+   ... pass
+   ...
+   >>> class Cat(Animal):
+   ... pass
+   ...
+   >>> class Bird:
+   ... pass
+   ...
+   >>> obj1 = Dog()
+   >>> obj2 = Cat()
+   >>> obj3 = Bird()
+   >>>
+   >>> inherits_from(obj1, Animal)
+   True
+   >>> inherits_from(obj2, Animal)
+  True
+  >>> inherits_from(obj3, Animal)
+ False
+ """
 
 
 def inherits_from(obj, a_class):
