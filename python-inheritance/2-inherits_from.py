2b2b160ff7c832055ee061793a392152143a82fd
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-05 14:28:22 +0100

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,36 +3,36 @@
    also if it is inherited from the specified class
 
    parameters:
-      obj: any python object.
-      a_class: a python class or class name to compare the type of the object against.
+   obj: any python object.
+   a_class: a python class or class name to compare the type
+   of the object against.
 
    returns:
    true if the object is an instance of the subclass of the specified subclass;
    otherwise, false.
    example:
-       >>> class Animal:
-       ...  pass
-       ...
-       >>> class Dog(Animal):
-       ... pass
-       ...
-      >>> class Cat(Animal):
-      ... pass
-      ...
-      >>> class Bird:
-      ... pass
-      ...
-      >>> obj1 = Dog()
-      >>> obj2 = Cat()
-      >>> obj3 = Bird()
-      >>>
-      >>> inherits_from(obj1, Animal)
-     True
-     >>> inherits_from(obj2, Animal)
-     True
+    >>> class Animal:
+    ...  pass
+    ...
+    >>> class Dog(Animal):
+    ... pass
+    ...
+    >>> class Cat(Animal):
+    ... pass
+    ...
+    >>> class Bird:
+    ... pass
+    ...
+    >>> obj1 = Dog()
+    >>> obj2 = Cat()
+    >>> obj3 = Bird()
+    >>>
+    >>> inherits_from(obj1, Animal)
+    True
+    >>> inherits_from(obj2, Animal)
+    True
     >>> inherits_from(obj3, Animal)
- 
-"""
+ """
 
 
 def inherits_from(obj, a_class):
