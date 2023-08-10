fde296e0ca6af1a5f65f010d6ec023bc1476d07f
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-10 22:34:15 +0100

    base class

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -32,6 +32,8 @@ class BaseGeometryMetaClass(type):
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
 
 class BaseGeometry(metaclass=BaseGeometryMetaClass):
     """
