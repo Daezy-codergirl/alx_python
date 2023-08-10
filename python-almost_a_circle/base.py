1aafef2b7fdd3c79bda045caee4adc8f695351c9
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-10 22:53:37 +0100

    base class

---
+++ python-almost_a_circle/base.py
@@ -0,0 +1,27 @@
+"""
+defining a class that has both private and instances
+"""
+
+class Base:
+    """
+    creating our first class.
+    """
+    """
+    A private class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        function __init__ calling self and an id
+        """
+        self.id = None
+
+        """
+        initialize the instance with id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
