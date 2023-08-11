e4307e5ac5898a8a2838be384922c0f736838a6a
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-11 14:55:29 +0100

    the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,6 +7,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         #size will stand for width & height
         
-        super().__init__(size, size, x=0, y=0, id=None)
+        super().__init__(size, size, x, y, id)
         # I won't change width and height to size because size isn't defined due to inheritance
         return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
