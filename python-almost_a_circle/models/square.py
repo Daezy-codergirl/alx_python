d026460d9b6b01c19e4c148103f47cbe206b19bc
Daezy-codergirl <122308807+Daezy-codergirl@users.noreply.github.com>
2023-08-12 14:28:56 +0100

    its a square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,21 +1,38 @@
-from rectangle import Rectangle
+"""
+Importing attributes from rectangle.py
+"""
+
+from models.rectangle import Rectangle
 """
 the class Square is inheriting from Rectangle
 """
 
 class Square(Rectangle):
+    """
+    Initializing the attributes
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        #size will stand for width & height
-        
+        """
+        size will stand for width & height
+        """
         super().__init__(size, size, x, y, id)
-        # I won't change width and height to size because size isn't defined due to inheritance
-        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def __str__(self):
+        """
+        I won't change width and height to size because size isn't defined due to inheritance
+        """
+        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width)
     
     @property
     def size(self):
-     # getter method
-      return self.width
+        """
+        getter method
+        """
+        return self.width
 
     @size.setter
     def size(self, value):
+        """
+        setter method
+        """
         self.width = value
